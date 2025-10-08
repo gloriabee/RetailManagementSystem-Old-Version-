f92b5ca9cd19ca340a39d7b768feb9a9505279cc
Figma beebee <nawgloriawinnyunt_n@cmu.ac.th>
2025-10-08 18:50:06 +0700

    Add project files.

---
+++ RetailManagementSystem.WPF/ViewModels/HomeVM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailManagementSystem.WPF.ViewModels
+{
+    public class HomeVM
+    {
+    }
+}
